a2be7389ded66134d14504abd7e591b6177beff3
Jure Sunic <juresunic@hotmail.com>
2022-06-01 19:31:38 +0100

    Initial Commit

---
+++ JureCard/JureCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JureCardApp.swift
+//  JureCard
+//
+//  Created by Jure Šunić on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct JureCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
